2b4ea00dc047b6b19cb6d85a01c9e15771682486
Maria89698 <maria9parhomova@gmail.com>
2023-02-11 09:06:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,19 +10,12 @@ for (let i; i <= 5; i += 1) {
 // # 3
 let num = 20;
 if (num < 28) {
-    let num = num + 1;
+    num = num += 1;
     console.log(21);
-    console.log(22);
-    console.log(23);
-    console.log(24);
-    console.log(25);
-    console.log(26);
-    console.log(27);
-    console.log(28);   
 }
 
 // # 4
-let name = prompt("Please, enter your name");
 for (let name; name.length < 6;) {
+    let name = prompt("Please, enter your name");
     console.log(name);
 }
